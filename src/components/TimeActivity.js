22d858256cc936d7e9415dc349c5517898a88f1c
Shubham Maurya <shubhammaurya20121998@gmail.com>
2017-07-05 20:43:44 +0530

    Create TimeActivity.js

---
+++ src/components/TimeActivity.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+class TimeActivity extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+        hii this will contain acitvity of time form
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default TimeActivity;
